cc208813d2e3ec48c21ceac9303b1eafe8db90f3
Ash <ashwinirao.karai@gmail.com>
2023-02-19 01:28:55 -0700

    Completed exercise-6

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -2,6 +2,17 @@ package com.serenitydojo.model;
 
 public class Feeder {
     public String feeds(String animal, boolean isPremium) {
-        return "Food";
+        if(animal.equals("Dog")) {
+           String premiumDogOrNot = (isPremium == true) ? "Deluxe Dog Food" : "Dog Food";
+           return premiumDogOrNot;
+        } else if (animal.equals("Hamster")) {
+            String premiumHamsterOrNot = (isPremium == true) ? "Lettuce" : "Cabbage";
+            return premiumHamsterOrNot;
+        }  else if (animal.equals("Cat")) {
+            String premiumCatOrNot = (isPremium == true) ? "Salmon" : "Tuna";
+            return premiumCatOrNot;
+        } else {
+            return "Unknown food";
+        }
     }
 }
